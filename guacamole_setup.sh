f707f94d3060b84d63a0bcfc923a797662cb6e82
devd4n <daniel-schmitt-it@gmx.de>
2022-03-12 20:49:11 +0100

    Update guacamole_setup.sh

--- guacamole_setup.sh
+++ guacamole_setup.sh
@@ -36,7 +36,7 @@ echo "DOCKER_GUACAMOLE_POSTGRES_PWD=$password" > ./.env
 
 mkdir -p ./postgresql
 wget -O ./$NAME_POSTGRES_JDBC $PATH_POSTGRES_JDBC
-mv ./$NAME_POSTGRES_JDBC ./postgres/
+mv ./$NAME_POSTGRES_JDBC ./postgresql/
 wget -O ./guacamole-auth-jdbc-1.4.0.tar.gz https://apache.org/dyn/closer.lua/guacamole/1.4.0/binary/guacamole-auth-jdbc-1.4.0.tar.gz?action=download
 tar xvfz ./guacamole-auth-jdbc-1.4.0.tar.gz
 mv ./guacamole-auth-jdbc-1.4.0/sqlserver/guacamole-auth-jdbc-sqlserver-1.4.0.jar ./postgresql/
